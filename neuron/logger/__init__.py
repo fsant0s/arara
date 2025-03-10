4f477aebfd2b0a30acab07e75fca2adf955d8d69
Sildolfo Gomes <sindolfogomes@gmail.com>
2025-03-10 18:09:14 -0300

    chore: update dependencies and documentation for monitoring system

--- neuron/logger/__init__.py
+++ neuron/logger/__init__.py
@@ -1,4 +1,26 @@
-from .file_logger import FileLogger
-from .logger_factory import LoggerFactory
+"""
+Logging package for the NEURON framework.
 
-__all__ = ("LoggerFactory", "FileLogger")
+This package provides logging utilities for different components of the framework,
+including file-based logging, database logging, and enhanced contextual logging.
+"""
+
+from .logger_factory import LoggerFactory
+from .base_logger import BaseLogger
+from .file_logger import FileLogger
+from .enhanced_logger import (
+    ContextualLogger,
+    get_logger,
+    configure_all_loggers,
+    log_operation
+)
+
+__all__ = [
+    'LoggerFactory',
+    'BaseLogger',
+    'FileLogger',
+    'ContextualLogger',
+    'get_logger',
+    'configure_all_loggers',
+    'log_operation'
+]
